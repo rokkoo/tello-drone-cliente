30bbec853893a791ccad99027e383656c2c0019f
Alfonso <alfonsolasaguirre@gmail.com>
2019-02-18 22:04:27 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+
+
+import StatusBar from './StatusBar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+      <StatusBar />
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +24,7 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
       </div>
     );
   }
---
+++ src/StatusBar.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { Button, Container } from 'semantic-ui-react'
+import axios from 'axios';
+import qs from 'qs'
+// Fetch drone information
+export default class StatusBar extends Component {
+    fetchInformation = async (cmd = 'battery?') => {
+        const config = {
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded'
+            }
+          }
+    
+        // wait for response
+        await axios.post(`http://localhost:3020/`, qs.stringify({
+            cmd
+        }), config)
+    }
+    render(){
+        return(
+            <div
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+
+            }}
+            >
+                <Button.Group>
+                    <Button onClick={(e) => this.fetchInformation('takeoff')}>Despegar</Button>
+                    <Button.Or />
+                    <Button positive onClick={(e) => this.fetchInformation('land')}>Atterrizar</Button>
+                </Button.Group>
+                <div style={{paddingTop: 10, textAlign: 'center'}}>
+                    <Button negative onClick={e => this.fetchInformation('emergency')}>Emergencia</Button>
+                </div>
+            </div>
+        )
+    }
+}
