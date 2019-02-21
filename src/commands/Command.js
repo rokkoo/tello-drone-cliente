2cf8489ebcfffe010f4d1bab2ace667a83b16b8c
Alfonso <alfonsolasaguirre@gmail.com>
2019-02-22 00:18:15 +0100

    Crado state para la informacion

--- src/commands/Command.js
+++ src/commands/Command.js
@@ -5,41 +5,37 @@ import './app.css'
 import { SocketContext } from '../fetch'
 
 export default function  Command({ text, iconName, backgroundColor = '#cd84f1', command }) {
-    if(iconName) {
-        return(
-            <SocketContext.Consumer>
-            {context => (
-                <div
-                style = {{
-                    backgroundColor
-                }}
-                className="commandContainer"
-                onClick={context.sendCommand.bind(this, command)}
-                >
-                    <Icon 
-                        name={iconName}
-                        size='large'
-                    />
-                    <p>{text}</p>
-                </div>
-            )}
-        </SocketContext.Consumer>
-        )
-    }else {
-        return(
-            <SocketContext.Consumer>
-            {context => (
-                <div
-                style = {{
-                    backgroundColor
-                }}
-                className="commandContainer"
-                onClick={context.sendCommand.bind(this, command)}
-                >
-                    <p>{text}</p>
-                </div>
-            )}
-        </SocketContext.Consumer>
-        )
-    }
+    return iconName 
+    ? (<SocketContext.Consumer>
+        {context => (
+            <div
+            style = {{
+                backgroundColor
+            }}
+            className="commandContainer"
+            onClick={context.sendCommand.bind(this, command)}
+            >
+                <Icon 
+                    name={iconName}
+                    size='large'
+                />
+                <p>{text}</p>
+            </div>
+        )}
+    </SocketContext.Consumer>)
+    : (
+        <SocketContext.Consumer>
+        {context => (
+            <div
+            style = {{
+                backgroundColor
+            }}
+            className="commandContainer"
+            onClick={context.sendCommand.bind(this, command)}
+            >
+                <p>{text}</p>
+            </div>
+        )}
+    </SocketContext.Consumer>
+    )
 }
--- src/fetch/socket-context.js
+++ src/fetch/socket-context.js
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 import socket from './SocketConnection'
 
-const context =  React.createContext({
-    droneData: {},
+const Context =  React.createContext({
+    droneData: [],
     name: 'prueba',
     sendCommand: command => {
         socket.emit('command', command)
         console.log('Send command', command)
-    }
+    },
 })
 
-export default context
+//TODO: hacer un state para las informacion del dron
+class GlobalState extends React.Component {
+    state =  {
+        dronData: [{battery: 'battery'}]
+    }
+    render() {
+        socket.on('droneInformation', (data) => {
+            console.log(data)
+        })
+
+        socket.on('droneInformation', function(data) {
+            console.log(data)
+        })
+        return (
+            <Context.Provider
+            value={{
+                droneData: this.state.dronData
+            }}
+            >
+            {this.props.children}
+            </Context.Provider>
+        )
+    }
+}
+
+export default Context
